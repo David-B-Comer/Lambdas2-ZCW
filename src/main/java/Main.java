a039a22346940b586f8c1b2946cb05ac45601947
David Comer <50469980+David-B-Comer@users.noreply.github.com>
2020-03-25 18:27:10 -0400

    saving for now

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+import java.util.List;
+
+public class Main implements CheckPerson{
+
+    public static void printPersons(
+            List<Person> roster, CheckPerson tester) {
+        for (Person p : roster) {
+            if (tester.test(p)) {
+                p.printPerson();
+            }
+        }
+    }
+
+    @Override
+    public boolean test(Person p) {
+        return false;
+    }
+}
